064288bf586cd3dd3346b499a95abcfe5bbe2102
Helen <helen.andrianova@gmail.com>
2022-12-29 14:20:31 +0100

    tmp scripts

--- giverSettings/index.ts
+++ giverSettings/index.ts
@@ -136,18 +136,42 @@ const testnetGiverAbi = {
   events: []
 } as const;
 
-export class GiverWalletV2 implements Giver {
-  constructor(
-    private readonly ever: ProviderRpcClient,
-    readonly keyPair: Ed25519KeyPair,
-    private readonly address: string,
-  ) {}
-  sendTo(sendTo: Address, value: string) {
-    return this.ever.sendMessage({
-      sender: new Address(this.address),
-      recipient: sendTo,
-      amount: value, // 10 EVER
-      bounce: false,
-    });
-  }
+export class GiverWalletV2_3 implements Giver {
+    public giverContract: Contract<typeof broxusEverWallet>;
+
+    constructor(ever: ProviderRpcClient, readonly keyPair: Ed25519KeyPair, address: string) {
+        const giverAddr = new Address(address);
+        this.giverContract = new ever.Contract(broxusEverWallet, giverAddr);
+    }
+
+    public async sendTo(sendTo: Address, value: string): Promise<{ transaction: Transaction; output?: {} }> {
+        return this.giverContract.methods
+            .sendTransaction({
+                value: value,
+                dest: sendTo,
+                bounce: false,
+                flags: 3,
+                payload: "",
+            })
+            .sendExternal({ publicKey: this.keyPair.publicKey });
+    }
 }
+const broxusEverWallet = {
+    "ABI version": 2,
+    version: "2.3",
+    header: ["pubkey", "time", "expire"],
+    functions: [
+        {
+            name: "sendTransaction",
+            inputs: [
+                { name: "dest", type: "address" },
+                { name: "value", type: "uint128" },
+                { name: "bounce", type: "bool" },
+                { name: "flags", type: "uint8" },
+                { name: "payload", type: "cell" },
+            ],
+            outputs: [],
+        },
+    ],
+    events: [],
+} as const;

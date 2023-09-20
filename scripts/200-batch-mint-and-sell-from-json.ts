a37b44e9253308ab89aaec90fe9dbde95f8706e6
Nikita Vagin <nikita_vagin@epam.com>
2023-09-20 06:02:00 +0500

    wip: script 200

--- scripts/200-batch-mint-and-sell-from-json.ts
+++ scripts/200-batch-mint-and-sell-from-json.ts
@@ -1,204 +1,208 @@
-import { isValidEverAddress} from "../test/utils";
+import { isValidEverAddress } from "../test/utils";
 import { Migration } from "./migration";
-import {Contract, toNano} from "locklift";
-import {Address} from "everscale-inpage-provider";
-import {FactoryDirectSell} from "../test/wrappers/directSell";
-import {CollectionSimilarAbi} from "../build/factorySource";
+import { Contract, toNano } from "locklift";
+import { Address } from "everscale-inpage-provider";
+import { FactoryDirectSell } from "../test/wrappers/directSell";
+import { CollectionSimilarAbi } from "../build/factorySource";
 
 const migration = new Migration();
-const BigNumber = require('bignumber.js');
-const fs = require('fs');
-const prompts = require('prompts');
+const BigNumber = require("bignumber.js");
+const fs = require("fs");
+const prompts = require("prompts");
 const logger = require("mocha-logger");
-const request = require('request');
+const request = require("request");
 
 const STEP = 2000;
-const FACTORY_DIRECT_SELL = new Address('0:d1f0569cb203e11a701b2e8bba65c3c79635367ce12c2ee563cf3281c72841e6');
-const COLLECTIONS_DATA = [{
-    name: 'Aquatech',
-    collection: new Address('0:7eb6488246ba08f88fe8779e9257ca9ebc7d2f82f6111ce6747abda368e3c7a8'),
-    mintAndSell: new Address('0:47f392241234f101b9081747739ce465b99bfa4da24c80932dbdb918dcaf3506')
-},{
-    name: 'Peachy Sands',
-    collection: new Address('0:33a630f9c54fc4092f43ab978f3fd65964bb0d775553c16953aa1568eb63ab0f'),
-    mintAndSell: new Address('0:0e174bf4871c10d3be4f1507b2125518de1f68af036cd9ce350a0fc108624d6e')
-},{
-    name: 'The Bubblemotes',
-    collection: new Address('0:d62691c79f447f512d7ad235a291435a8a886debff1b72dfc3ff5e486798d96e'),
-    mintAndSell: new Address('0:a3efca5e3d472928107a9459d7a40dd1d3b42a4881de9f751eb1e917362e401e')
-},{
-    name: 'Absrart Collection',
-    collection: new Address('0:ec0ab798c85aa7256865221bacd4f3df220cf60277a2b79b3091b76c265d1cd7'),
-    mintAndSell: new Address('0:f9a92fdb7a38f1f54c3b962d900cbf0057c83da4462fe8149f2cb76e8c7f0caa')
-}];
+//const FACTORY_DIRECT_SELL = new Address("0:3158c265c052f093b0d9bd799b74dbc454ee35a79501bd599254997bc5e92b73");
+const loadDirectSell = migration.loadContract("FactoryDirectSell", "FactoryDirectSell");
+const FACTORY_DIRECT_SELL = loadDirectSell.address;
+// const collection = {
+//   name: "DiscoDance",
+//   collection: new Address("0:dc7523fa70eb5d83cb70b366b8e0d8bdf9cf4a8a9a2fdab00a9bcf9420874be4"),
+//   mintAndSell: new Address("0:99c7432fc154bb5bfc04ac65c2f7a92bfa66dec9f673302124784e5174ae9b78"),
+// };
+const loadCollection = migration.loadContract("CollectionSimilar", "SimilarCollection");
+const loadMintAndSell = migration.loadContract("MintAndSell", "MintAndSell");
+const collection = {
+  name: "DiscoDance",
+  collection: loadCollection.address,
+  mintAndSell: loadMintAndSell.address,
+};
 
 function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function waitOfferCreated(collection: Address, nftId: number) {
-    const collectionSimilar: Contract<CollectionSimilarAbi> = await locklift.factory.getDeployedContract(
-        "CollectionSimilar",
-        collection
-    );
-    let {nft: nftAddress} = (await collectionSimilar.methods.nftAddress({answerId: 0, id: nftId}).call());
-    const Nft = await locklift.factory.getDeployedContract("Nft", nftAddress);
-    let manager = (await Nft.methods.getInfo({answerId: 0}).call()).manager.toString();
-    let owner = (await Nft.methods.getInfo({answerId: 0}).call()).owner.toString();
+  const collectionSimilar: Contract<CollectionSimilarAbi> = await locklift.factory.getDeployedContract(
+    "CollectionSimilar",
+    collection,
+  );
+  let { nft: nftAddress } = await collectionSimilar.methods.nftAddress({ answerId: 0, id: nftId }).call();
+  const Nft = await locklift.factory.getDeployedContract("Nft", nftAddress);
+  let manager = (await Nft.methods.getInfo({ answerId: 0 }).call()).manager.toString();
+  let owner = (await Nft.methods.getInfo({ answerId: 0 }).call()).owner.toString();
 
-    if (owner == manager) {
-        console.log(`Nft(${nftAddress}) not on sale.`);
-        await sleep(30000);
-        await waitOfferCreated(collection, nftId);
-    } else {
-        console.log(`Nft(${nftAddress}) ON SALE. DirectSell/DirectSellFactory = ${manager}`);
-    }
+  if (owner == manager) {
+    console.log(`Nft(${nftAddress}) not on sale.`);
+    await sleep(30000);
+    await waitOfferCreated(collection, nftId);
+  } else {
+    console.log(`Nft(${nftAddress}) ON SALE. DirectSell/DirectSellFactory = ${manager}`);
+  }
 }
 
 async function isNftDeployed(collection: Address, nftId: number) {
-    const collectionSimilar: Contract<CollectionSimilarAbi> = await locklift.factory.getDeployedContract(
-        "CollectionSimilar",
-        collection
-    );
-    let {nft: nftAddress} = (await collectionSimilar.methods.nftAddress({answerId: 0, id: nftId}).call());
-
-    const isDeployed = (await locklift.provider.getFullContractState({address: nftAddress})).state?.isDeployed;
-    if(isDeployed) {
-        console.log(`NFT(${nftAddress}) DEPLOYED`);
-    } else {
-        console.log(`NFT(${nftAddress}) not deployed`);
-    }
-    return isDeployed;
+  const collectionSimilar: Contract<CollectionSimilarAbi> = await locklift.factory.getDeployedContract(
+    "CollectionSimilar",
+    collection,
+  );
+  let { nft: nftAddress } = await collectionSimilar.methods.nftAddress({ answerId: 0, id: nftId }).call();
+  const isDeployed = (await locklift.provider.getFullContractState({ address: nftAddress })).state?.isDeployed;
+  if (isDeployed) {
+    console.log(`NFT(${nftAddress}) DEPLOYED`);
+  } else {
+    console.log(`NFT(${nftAddress}) not deployed`);
+  }
+  return isDeployed;
 }
 
 async function waitNftMinted(collection: Address, nftId: number) {
-    const lastId = await getLastNftId(collection);
-    const nftDeployed = await isNftDeployed(collection, nftId);
+  const lastId = await getLastNftId(collection);
+  const nftDeployed = await isNftDeployed(collection, nftId);
 
-    if (lastId < nftId || !nftDeployed) {
-        console.log(`Last event nft id: ${lastId}/${nftId}`);
-        await sleep(30000);
-        await waitNftMinted(collection, nftId);
-    } else {
-        console.log(`All minted`);
-    }
-
+  if (lastId < nftId || !nftDeployed) {
+    console.log(`Last event nft id: ${lastId}/${nftId}`);
+    await sleep(30000);
+    await waitNftMinted(collection, nftId);
+  } else {
+    console.log(`All minted`);
+  }
 }
 
 async function getLastNftId(collection: Address) {
-        const collectionSimilar: Contract<CollectionSimilarAbi> = await locklift.factory.getDeployedContract(
-            "CollectionSimilar",
-            collection
-        )
-        const lastNFTCreated = ((await collectionSimilar.getPastEvents<'NftCreated'>({
-            limit: 1,
-            filter: (event) => event.event === 'NftCreated'
-        })).events).shift();
-        return parseInt(lastNFTCreated!.data.id);
+  const collectionSimilar: Contract<CollectionSimilarAbi> = await locklift.factory.getDeployedContract(
+    "CollectionSimilar",
+    collection,
+  );
+  const lastNFTCreated = (
+    await collectionSimilar.getPastEvents<"NftCreated">({
+      limit: 1,
+      filter: event => event.event === "NftCreated",
+    })
+  ).events.shift();
+  return lastNFTCreated?.data?.id ? parseInt(lastNFTCreated?.data?.id) : 0;
 }
 
 async function main() {
-    const signer = (await locklift.keystore.getSigner('0'));
-    const account = await migration.loadAccount('Account1');
+  const signer = await locklift.keystore.getSigner("0");
+  const account = await migration.loadAccount("Account1");
 
-        const response = await prompts([
-        {
-            type: 'number',
-            name: 'from',
-            message: 'Get NFT from count (default 1)',
-            initial: 1
+  const response = await prompts([
+    {
+      type: "number",
+      name: "from",
+      message: "Get NFT from count (default 1)",
+      initial: 1,
+    },
+    {
+      type: "number",
+      name: "to",
+      message: "Get NFT to count (default 10)",
+      initial: 10,
+    },
+  ]);
+
+  let current = response.from;
+
+  while (current < response.to) {
+    const to = Math.min(current + STEP, response.to);
+
+    const lastNFTCreated = await getLastNftId(collection.collection);
+
+    if (lastNFTCreated < to) {
+      const mintAndSell = await locklift.factory.getDeployedContract("MintAndSell", collection.mintAndSell);
+
+      const from: number = Math.max(lastNFTCreated, current) + 1;
+
+      console.log(
+        `Start Mint NFT for collection 
+        ${collection.name} - ${collection.collection.toString()}. 
+        MintAndSell - ${collection.mintAndSell}. 
+        from = ${from}, to = ${to}`,
+      );
+      await mintAndSell.methods.createItems({ _fromId: from, _toId: to }).send({
+        from: account.address,
+        amount: new BigNumber(to).minus(from).plus(1).times(1.2).plus(1).shiftedBy(9).toString(),
+      });
+      await waitNftMinted(collection.collection, to);
+      console.log(
+        `All NFT minted for collection ${
+          collection.name
+        } - ${collection.collection.toString()}. from = ${from}, to = ${to}`,
+      );
+
+      console.log(`Update metadata`);
+      let options = {
+        method: "POST",
+        url: "https://indexer-venom.bf.works/metadata/refresh/",
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-            type: 'number',
-            name: 'to',
-            message: 'Get NFT to count (default 10)',
-            initial: 10
-        }
-    ]);
+        body: JSON.stringify({
+          collection: collection.collection.toString(),
+        }),
+      };
+      request(options, function (error: any, response: any) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+      });
 
-    let current = response.from;
+      console.log(
+        `Start sell minted NFTs for collection ${
+          collection.name
+        } - ${collection.collection.toString()}. from = ${from}, to = ${to}`,
+      );
+      const factoryDirectSell = await locklift.factory.getDeployedContract("FactoryDirectSell", FACTORY_DIRECT_SELL);
+      const gas = (await factoryDirectSell.methods.getGasValue().call()).value0;
+      const gasPrice = new BigNumber(1).shiftedBy(9).div(gas.gasK);
+      let targetGas = new BigNumber(gas.sell.dynamicGas).times(gasPrice).plus(gas.sell.fixedValue).toNumber();
 
-    while(current < response.to ) {
-        const to = Math.min(current + STEP, response.to);
+      console.log(`MintAndSell('${mintAndSell.address}').sellItems({_fromId: ${from}, _toId: ${to}})`);
+      let transaction = await mintAndSell.methods.sellItems({ _fromId: from, _toId: to }).send({
+        from: account.address,
+        amount: new BigNumber(targetGas)
+          .shiftedBy(-9)
+          .plus(0.4)
+          .times(to - from + 1)
+          .plus(1)
+          .shiftedBy(9)
+          .toString(),
+      });
+      console.log(`tx: ${transaction.id.hash}`);
 
-        for( let i = 0; i < COLLECTIONS_DATA.length; i++ ) {
-            const data = COLLECTIONS_DATA[i];
+      console.log("Start check offer created");
+      await waitOfferCreated(collection.collection, to);
+      console.log(
+        `All NFTs was sell for collection ${
+          collection.name
+        } - ${collection.collection.toString()}. from = ${from}, to = ${to}`,
+      );
 
-            const lastNFTCreated = await getLastNftId(data.collection);
-
-            if (lastNFTCreated < to) {
-
-                const mintAndSell = await locklift.factory.getDeployedContract(
-                    "MintAndSell",
-                    data.mintAndSell
-                );
-
-                const from: number = Math.max(lastNFTCreated, current) + 1;
-
-                console.log(`Start Mint NFT for collection ${data.name} - ${data.collection.toString()}.MintAndSell - ${data.mintAndSell}. from = ${from}, to = ${to}`);
-                await mintAndSell.methods.createItems({_fromId: from, _toId: to}).send({
-                    from: account.address,
-                    amount: new BigNumber(to).minus(from).plus(1).times(1.2).plus(1).shiftedBy(9).toString()
-                });
-
-                await waitNftMinted(data.collection, to);
-                console.log(`All NFT minted for collection ${data.name} - ${data.collection.toString()}. from = ${from}, to = ${to}`);
-
-                console.log(`Update metadata`);
-                let options = {
-                  'method': 'POST',
-                  'url': 'https://indexer-venom.bf.works/metadata/refresh/',
-                  'headers': {
-                    'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({
-                    "collection": data.collection.toString()
-                  })
-
-                };
-                request(options, function (error: any, response: any) {
-                  if (error) throw new Error(error);
-                  console.log(response.body);
-                });
-
-                console.log(`Start sell minted NFTs for collection ${data.name} - ${data.collection.toString()}. from = ${from}, to = ${to}`);
-                const factoryDirectSell = await locklift.factory.getDeployedContract(
-                    "FactoryDirectSell",
-                    FACTORY_DIRECT_SELL
-                );
-                const gas = (await factoryDirectSell.methods.getGasValue().call()).value0;
-                const gasPrice = new BigNumber(1).shiftedBy(9).div(gas.gasK);
-                let targetGas = new BigNumber(gas.sell.dynamicGas).times(gasPrice).plus(gas.sell.fixedValue).toNumber();
-
-                console.log(`MintAndSell('${mintAndSell.address}').sellItems({_fromId: ${from}, _toId: ${to}})`);
-                let transaction  = await mintAndSell.methods.sellItems({_fromId: from, _toId: to}).send({
-                    from: account.address,
-                    amount: new BigNumber(targetGas).shiftedBy(-9).plus(0.4).times(to - from + 1)
-                        .plus(1)
-                        .shiftedBy(9).toString()
-                });
-                console.log(`tx: ${transaction.id.hash}`);
-
-                console.log('Start check offer created');
-                await waitOfferCreated(data.collection, to);
-                console.log(`All NFTs was sell for collection ${data.name} - ${data.collection.toString()}. from = ${from}, to = ${to}`);
-
-                console.log(`Drain gas for MintAndSell - ${mintAndSell.address}`);
-                await mintAndSell.methods.drainGas({}).send({
-                    from: account.address,
-                    amount: toNano(0.1)
-                });
-            }
-
-        }
-        current = to;
+      console.log(`Drain gas for MintAndSell - ${mintAndSell.address}`);
+      await mintAndSell.methods.drainGas({}).send({
+        from: account.address,
+        amount: toNano(0.1),
+      });
     }
 
+    current = to;
+  }
 }
 
 main()
-.then(() => process.exit(0))
-.catch(e => {
+  .then(() => process.exit(0))
+  .catch(e => {
     console.log(e);
     process.exit(1);
-});
+  });
--- scripts/6-deploy-collection-and-mintAndsell.ts
+++ scripts/6-deploy-collection-and-mintAndsell.ts
@@ -1,161 +1,168 @@
-import { isValidEverAddress} from "../test/utils";
+import { isValidEverAddress } from "../test/utils";
 import { Migration } from "./migration";
-import {toNano} from "locklift";
-import {Address} from "everscale-inpage-provider";
-import {FactoryDirectSell} from "../test/wrappers/directSell";
+import { toNano } from "locklift";
+import { Address } from "everscale-inpage-provider";
+import { FactoryDirectSell } from "../test/wrappers/directSell";
+import { json } from "stream/consumers";
 
 const migration = new Migration();
-const BigNumber = require('bignumber.js');
-const fs = require('fs');
-const prompts = require('prompts');
+const BigNumber = require("bignumber.js");
+const prompts = require("prompts");
 const logger = require("mocha-logger");
-
 
 let collection_json: any;
 let patterns: any[] = [];
 
 async function main() {
-    const signer = (await locklift.keystore.getSigner('0'));
-    const account = await migration.loadAccount('Account1');
+  const signer = await locklift.keystore.getSigner("0");
+  const account = await migration.loadAccount("Account1");
+  await SimilarCollectionMigration(signer, account);
 
-    const savedFactoryDirectSell = await migration.loadContract('FactoryDirectSell', 'FactoryDirectSell');
+  const savedFactoryDirectSell = await migration.loadContract("FactoryDirectSell", "FactoryDirectSell");
 
-    const savedCollection = await migration.loadContract('CollectionSimilar', 'SimilarCollection');
+  const savedCollection = await migration.loadContract("CollectionSimilar", "SimilarCollection");
+  const response = await prompts([
+    {
+      type: "text",
+      name: "factoryDirectSell",
+      message: "Get FactoryDirectSell address (default " + savedFactoryDirectSell.address + ")",
+      validate: (value: any) => (isValidEverAddress(value) || value === "" ? true : "Invalid Everscale address"),
+      initial: savedFactoryDirectSell.address,
+    },
+  ]);
 
-    const response = await prompts([
-        // {
-        //     type: 'text',
-        //     name: 'nftOwner',
-        //     message: 'Get NFT Owner Address - recipient (default ' + account.address + ')',
-        //     validate: (value: any) => isValidEverAddress(value) || value === '' ? true : 'Invalid Everscale address',
-        //     initial: account.address
-        // },
-        {
-            type: 'text',
-            name: 'factoryDirectSell',
-            message: 'Get FactoryDirectSell address (default ' + savedFactoryDirectSell.address + ')',
-            validate: (value: any) => isValidEverAddress(value) || value === '' ? true : 'Invalid Everscale address',
-            initial: savedFactoryDirectSell.address
-        },
-        {
-            type: 'text',
-            name: 'collection',
-            message: 'Get Collection address (default ' + savedCollection.address + ')',
-            validate: (value: any) => isValidEverAddress(value) || value === '' ? true : 'Invalid Everscale address',
-            initial: savedCollection.address
-        }
-        // {
-        //     type: 'text',
-        //     name: 'tokenRoot',
-        //     message: 'Get tokenRoot address',
-        //     validate: (value: any) => isValidEverAddress(value) || value === '' ? true : 'Invalid Everscale address',
-        // }
-    ]);
+  const factoryDirectSell = await locklift.factory.getDeployedContract(
+    "FactoryDirectSell",
+    new Address(response.factoryDirectSell.toString()),
+  );
+  const gas = (await factoryDirectSell.methods.getGasValue().call()).value0;
+  const gasPrice = new BigNumber(1).shiftedBy(9).div(gas.gasK);
+  let targetGas = new BigNumber(gas.sell.dynamicGas).times(gasPrice).plus(gas.sell.fixedValue).toNumber();
 
-    // const data = fs.readFileSync("collection4.json", 'utf8');
-    // if (data) collection_json = JSON.parse(data);
-
-    // deploy collection
-    logger.log("Deploy collection");
-    const Nft = (await locklift.factory.getContractArtifacts("Nft"));
-    const Index = (await locklift.factory.getContractArtifacts("Index"));
-    const IndexBasis = (await locklift.factory.getContractArtifacts("IndexBasis"));
-
-    if (collection_json.nfts) {
-        for (let nft of collection_json.nfts) {
-            patterns.push(
-                {
-                    name: nft.name,
-                    description: nft.description,
-                    previewUrl: nft.preview_url,
-                    previewMimeType: nft.previewMimeType,
-                    fileUrl: nft.url,
-                    fileMimeType: nft.fileMimeType,
-                    externalUrl: nft.externalUrl,
-                    attributes: nft.attributes
-                }
-            );
-        }
-    }
-
-    const collection = (await locklift.factory.deployContract({
-        contract: "CollectionSimilar",
-        publicKey: (signer?.publicKey) as string,
-        constructorParams: {
-            codeNft: Nft.code,
-            codeIndex: Index.code,
-            codeIndexBasis: IndexBasis.code,
-            owner: account.address,
-            remainOnNft: toNano(0.3),
-            json: JSON.stringify(collection_json.collection),
-            patterns: patterns,
-        },
-        initParams: {
-            nonce_: locklift.utils.getRandomNonce()
-        },
-        value: toNano(5)
-    })).contract;
-    logger.success(`Collection: ${collection.address}`);
-
-    migration.store(collection, "SimilarCollection");
-
-    const factoryDirectSell = await locklift.factory.getDeployedContract(
-        "FactoryDirectSell",
-        new Address(response.factoryDirectSell.toString())
-    );
-    const gas = (await factoryDirectSell.methods.getGasValue().call()).value0;
-    const gasPrice = new BigNumber(1).shiftedBy(9).div(gas.gasK);
-    let targetGas = new BigNumber(gas.sell.dynamicGas).times(gasPrice).plus(gas.sell.fixedValue).toNumber();
-
-    // build payload for directSell
-    logger.log("Build payload");
-    const targetPayload = 'te6ccgEBAgEArgABzQAAAAAAAAAAZErH4AAAAAAAAAAAgAUEb0urV2ZIJ082vzCuYOmnZzbkJCYLs5OS8iX4DbCHgAAAAAAAAAAAAAAAB3NZQBACvzJmcKhrCAR1ZqNCY+f5BTbr+AsLAVzbOgGvk5rzfXYBAIOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABA=';
-    /*(await factoryDirectSell.methods.buildDirectSellCreationPayload({
-        callbackId: 0,
-        _startTime: Math.round(Date.now() / 1000),
-        durationTime: 0,
-        _paymentToken: response.tokenRoot,
+  // build payload for directSell
+  logger.log("Build payload");
+  const targetPayload = (
+    await factoryDirectSell.methods
+      .buildDirectSellCreationPayload({
+        _callbackId: 0,
+        _startTime: 1695160632,
+        _durationTime: 0,
+        _paymentToken: new Address("0:2c3a2ff6443af741ce653ae4ef2c85c2d52a9df84944bbe14d702c3131da3f14"),
         _price: toNano(1),
-        recipient: response.nftOwner,
+        _recipient: new Address("0:afcc999c2a1ac2011d59a8d098f9fe414dbafe02c2c05736ce806be4e6bcdf5d"),
+        _discountCollection: null,
+        _discountNftId: null,
+      })
+      .call()
+  ).value0;
+  //deploy mintAndSell
+  logger.log("Deploy mintAndSell");
+  const mintAndSell = (
+    await locklift.factory.deployContract({
+      contract: "MintAndSell",
+      publicKey: signer?.publicKey as string,
+      constructorParams: {
+        _owner: account.address,
+        _collection: savedCollection.address,
+        _targetManager: factoryDirectSell.address,
+        _targetGas: targetGas,
+        _targetPayload: targetPayload,
+      },
+      initParams: {
+        nonce_: locklift.utils.getRandomNonce(),
+      },
+      value: toNano(1.6),
+    })
+  ).contract;
+  logger.success(`MintAndSell: ${mintAndSell.address}`);
+  // Transfer collection ownership to MintAndSell
+  await locklift.tracing.trace(
+    savedCollection.methods
+      .transferOwnership({ newOwner: mintAndSell.address })
+      .send({ from: account.address, amount: toNano(1) }),
+  );
+  logger.success("Transfer collection ownership to MintAndSell");
 
-    }).call()).value0;*/
-
-
-    //deploy mintAndSell
-    logger.log("Deploy mintAndSell");
-    const mintAndSell = (await locklift.factory.deployContract({
-        contract: "MintAndSell",
-        publicKey: (signer?.publicKey) as string,
-        constructorParams: {
-            _owner: account.address,
-            _collection: response.collection,
-            _targetManager: factoryDirectSell.address,
-            _targetGas: targetGas,
-            _targetPayload: targetPayload
+  migration.store(mintAndSell, "MintAndSell");
+}
+async function SimilarCollectionMigration(signer: any, account: any) {
+  const Nft = await locklift.factory.getContractArtifacts("Nft");
+  const Index = await locklift.factory.getContractArtifacts("Index");
+  const IndexBasis = await locklift.factory.getContractArtifacts("IndexBasis");
+  collection_json = [
+    {
+      collection: {
+        type: "Basic NFT",
+        name: "DiscoDance",
+        description: "Just dance with me",
+        preview: {
+          source: "https://www.artmajeur.com/medias/standard/s/y/sybemol/artwork/16622434_dancing-cat07.jpg",
+          mimetype: "image/png",
         },
-        initParams: {
-            nonce_: locklift.utils.getRandomNonce()
+        files: [
+          {
+            source: "https://www.artmajeur.com/medias/standard/s/y/sybemol/artwork/16622434_dancing-cat07.jpg",
+            mimetype: "image/jpg",
+          },
+        ],
+        external_url: "",
+      },
+      nfts: [
+        {
+          preview_url: "https://www.artmajeur.com/medias/standard/s/y/sybemol/artwork/16622434_dancing-cat07.jpg",
+          previewMimeType: "image/png",
+          url: "https://www.artmajeur.com/medias/standard/s/y/sybemol/artwork/16622434_dancing-cat07.jpg",
+          fileMimeType: "image/png",
+          name: "DiscoCat",
+          description: "Dancing cat young and sweet only seventeen",
+          externalUrl: "",
+          attributes: [],
         },
-        value: toNano(1.6)
-    })).contract;
-    logger.success(`MintAndSell: ${mintAndSell.address}`);
+      ],
+    },
+  ];
+  // deploy collection
+  logger.log("Deploy collection");
 
-    migration.store(mintAndSell, "MintAndSell");
+  const nfts = collection_json[0].nfts;
 
-    // // Transfer collection ownership to MintAndSell
-    // logger.log("Transfer collection ownership to MintAndSell");
-    // await locklift.tracing.trace(collection.methods.transferOwnership({
-    //     newOwner: mintAndSell.address
-    // }).send({
-    //     from: account.address,
-    //     amount: toNano(1)
-    // }));
+  for (let nft of nfts) {
+    patterns.push({
+      name: nft.name,
+      description: nft.description,
+      previewUrl: nft.preview_url,
+      previewMimeType: nft.previewMimeType,
+      fileUrl: nft.url,
+      fileMimeType: nft.fileMimeType,
+      externalUrl: nft.externalUrl,
+      attributes: nft.attributes,
+    });
+  }
 
+  const { contract: collection, tx } = await locklift.factory.deployContract({
+    contract: "CollectionSimilar",
+    publicKey: signer?.publicKey as string,
+    constructorParams: {
+      codeNft: Nft.code,
+      codeIndex: Index.code,
+      codeIndexBasis: IndexBasis.code,
+      owner: account.address,
+      remainOnNft: locklift.utils.toNano(1),
+      json: JSON.stringify(collection_json[0].collection),
+      patterns: patterns,
+    },
+    initParams: {
+      nonce_: locklift.utils.getRandomNonce(),
+    },
+    value: locklift.utils.toNano(5),
+  });
+  logger.success(`Collection: ${collection.address}`);
+  migration.store(collection, "SimilarCollection");
 }
 
 main()
-.then(() => process.exit(0))
-.catch(e => {
+  .then(() => process.exit(0))
+  .catch(e => {
     console.log(e);
     process.exit(1);
-});
+  });
